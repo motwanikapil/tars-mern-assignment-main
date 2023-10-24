5dc8c52a457eefe1fb2fa6f69c00477ac190d827
Kapil Motwani <51946007+motwanikapil@users.noreply.github.com>
2023-10-24 06:39:26 +0000

    Update 2 files

---
+++ src/utils/Important.js
@@ -0,0 +1,5 @@
+const ACCESS_KEY = "1DCiDaaGrdadNpsQQEM9lfY-nxZPThwkU6zrAeCW4SU"
+const PRIVATE_KEY = "fW0D6C0DFhetZKXF_A7dNchL38pI0FZgiNCdQdDtEg0"
+const URL = "https://api.unsplash.com"
+
+module.exports = { ACCESS_KEY, PRIVATE_KEY, URL }
